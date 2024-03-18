969ade7a449fdd4d4c6f78be1072d96b55b7a043
BhargobLahon <lahonbhargab@gmail.com>
2024-03-18 20:12:04 +0530

    updating

---
+++ src/Components/Tracks/Tracks.jsx
@@ -0,0 +1,52 @@
+import styles from "./Tracks.css";
+
+const Tracks = () => {
+    return(
+        <div>
+        <div className='header'>
+            <h1><b>Tracks</b></h1>
+        </div>
+        <div className='container'>
+        <div className='left'>
+            <div id='lcard'>
+                <h3>TT1:</h3>
+                <h2>Power Converters, Devices, EMI/EMC and Packaging</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT3:</h3>
+                <h2>Transportation</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT5:</h3>
+                <h2>Renewable Energy Systems and Energy Storage</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT7:</h3>
+                <h2>Power Engineering Educations and Issues</h2>
+            </div>
+        </div>
+        <div className='right'>
+            <div id='lcard'>
+                <h3>TT2:</h3>
+            <h2>Electrical Machines and Industrial Drives</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT4:</h3>
+                <h2>Control and Automation</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT6:</h3>
+                <h2>Smart Grids & Power Quality</h2>
+            </div>
+            <div id='lcard'>
+                <h3>TT8:</h3>
+                <h2>Applications of AI and ML Techniques to Power Electronics</h2>
+            </div>
+        </div>
+        </div>
+        </div>
+    )
+
+
+}
+export default Tracks
