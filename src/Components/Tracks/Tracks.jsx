df2ca57983fa27d6c33eb81013028011bc362be9
BhargobLahon <lahonbhargab@gmail.com>
2024-03-24 00:09:55 +0530

    updating tracks about us

--- src/Components/Tracks/Tracks.jsx
+++ src/Components/Tracks/Tracks.jsx
@@ -15,35 +15,31 @@ const Tracks = () => {
             <h2>Power Conversion Technologies For Transportation Electification And Sustainable Energy Integration</h2>
           </div>
           <div className={styles.lcard}>
-            <h3 className=" font-semibold">Track 3:</h3>
-            <h2>Smart And Sustainable Charging Infrastructure For E-Mobility</h2>
-          </div>
-          <div className={styles.lcard}>
-            <h3 className=" font-semibold">Track 5:</h3>
-            <h2>Computational Intelligence, Cyber Security And Iot Applications For Energy Efficiency And Conservation</h2>
-          </div>
-          {/* <div className={styles.lcard}>
-            <h3 className=" font-semibold">Track 7:</h3>
-            <h2>Power Engineering Educations and Issues</h2>
-          </div> */}
-        </div>
-        <div className={styles.right}>
-          <div className={styles.lcard}>
             <h3 className=" font-semibold">Track 2:</h3>
             <h2>Modeling And Control Of Energy Efficienct Drives For Electic Transportation (LAND, AIR AND SEA)</h2>
           </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">Track 3:</h3>
+            <h2>Smart And Sustainable Charging Infrastructure For E-Mobility</h2>
+          </div>
+          
+         
+        </div>
+        <div className={styles.right}>
+          
           <div className={styles.lcard}>
             <h3 className=" font-semibold">Track 4:</h3>
             <h2>Energy Conversion And Management For Sustainable Smart Energy Systems And Rural Electification </h2>
           </div>
           <div className={styles.lcard}>
+            <h3 className=" font-semibold">Track 5:</h3>
+            <h2>Computational Intelligence, Cyber Security And Iot Applications For Energy Efficiency And Conservation</h2>
+          </div>
+          <div className={styles.lcard}>
             <h3 className=" font-semibold">Track 6:</h3>
             <h2>Automation, Control And Digitisation For Industry Applications</h2>
           </div>
-          {/* <div className={styles.lcard}>
-            <h3 className=" font-semibold">Track 8:</h3>
-            <h2>Applications of AI and ML Techniques to Power Electronics</h2>
-          </div> */}
+        
         </div>
       </div>
     </div>
