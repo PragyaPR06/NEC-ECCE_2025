34786e4a2beceb740b494d7cc6a126bc801c0e1c
BhargobLahon <lahonbhargab@gmail.com>
2024-03-23 22:26:20 +0530

    updating about us tracks responsive

--- src/Components/Patrons/patrons.jsx
+++ src/Components/Patrons/patrons.jsx
@@ -9,19 +9,21 @@ return(<div className={styles.container}>
       <img src="/patrons/director.jpg" alt="image" className={styles.card__img}/>
 
       <div className={styles.card__data}>
-         <span className={styles.card__description}>Director, NIT Silchar</span>
+         
          <h2 className={styles.card__title}>Prof. Dilip Kumar Baidya</h2>
-         <a href="#" className={styles.card__button}>Read More</a>
+         <span className={styles.card__description}>Director, NIT Silchar</span>
+         
       </div>
    </article>
 
    <article className={styles.card__article}>
-      <img src="/patrons/director.jpg" alt="image" className={styles.card__img}/>
+      <img src="/patrons/skch.jpeg" alt="image" className={styles.card__img}/>
 
       <div className={styles.card__data}>
-         <span className={styles.card__description}>Chair, IEEE Kolkata Section</span>
+         
          <h2 className={styles.card__title}>Prof. Suparna Kar Chowdhury</h2>
-         <a href="#" className={styles.card__button}>Read More</a>
+         <span className={styles.card__description}>Chair, IEEE Kolkata Section</span>
+        
       </div>
    </article>
 
@@ -31,32 +33,35 @@ return(<div className={styles.container}>
 <br></br><br></br>
 <div className={styles.card__container2}>
    <article className={styles.card__article}>
-      <img src="/patrons/director.jpg" alt="image" className={styles.card__img}/>
+      <img src="/patrons/nsinha.jpg" alt="image" className={styles.card__img}/>
 
       <div className={styles.card__data}>
-         <span className={styles.card__description}>NIT Silchar</span>
+        
          <h2 className={styles.card__title}>Prof. N. Sinha</h2>
-         <a href="#" className={styles.card__button}>Read More</a>
+         <span className={styles.card__description}>NIT Silchar</span>
+        
       </div>
    </article>
 
    <article className={styles.card__article}>
-      <img src="/patrons/director.jpg" alt="image" className={styles.card__img}/>
+      <img src="/patrons/fatalukdar.png" alt="image" className={styles.card__img}/>
 
       <div className={styles.card__data}>
-         <span className={styles.card__description}>Chair, IEEE Silchar Subsection</span>
+        
          <h2 className={styles.card__title}>Prof. F. A. Talukdar</h2>
-         <a href="#" className={styles.card__button}>Read More</a>
+         <span className={styles.card__description}>Chair, IEEE Silchar Subsection</span>
+         
       </div>
    </article>
 
    <article className={styles.card__article}>
-      <img src="/patrons/director.jpg" alt="image" className={styles.card__img}/>
+      <img src="/patrons/bk.jpg" alt="image" className={styles.card__img}/>
 
       <div className={styles.card__data}>
-         <span className={styles.card__description}> Chair-Elect., IEEE Silchar Subsection</span>
+        
          <h2 className={styles.card__title}>Prof. B. K. Roy</h2>
-         <a href="#" className={styles.card__button}>Read More</a>
+         <span className={styles.card__description}> Chair-Elect., IEEE Silchar Subsection</span>
+         
       </div>
    </article>
 </div>
