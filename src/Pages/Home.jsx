b7989ce303a6411d3b98c8558ea65fca56d4b9b2
BhargobLahon <lahonbhargab@gmail.com>
2024-03-24 15:28:53 +0530

    updating header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,15 +5,36 @@ import Navbar from "../Components/Navbar/Navbar"
 import AboutUsComponent from '../Components/About/About';
 import Timer from '../Components/Timer/Timer';
 import Patrons from '../Components/Patrons/patrons';
+import Header from '../Components/Header/header';
 const Home = () => {
   return (
     <div>
+    <Header/>
       <Navbar/>
       <HomeSlider />
       <AboutUsComponent/>
       <Tracks/>
       <Timer/>
       <Patrons/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
       
       
 
