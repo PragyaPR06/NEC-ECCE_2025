776360ee09befb457dcc00edb7777aa2a024f882
hradaysadrani <hradaysadrani@gmail.com>
2024-03-27 00:11:38 +0530

    changed size of logo

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -26,7 +26,7 @@ const Header =() => {
           <img src={require("./ne-iecce-2025_main_logo.jpg")} alt="ne-iecce-2025_logo" width="150rem" height="100rem"/>
         </div>
         <div className={styles.header_logo}>
-          <img src={require("./nits-logo.png")} alt="nits-logo" width="150rem" height="100rem"/> 
+          <img src={require("./nits-logo.png")} alt="nits-logo" width="130rem" height="90rem"/> 
         </div>
         <div className={styles.header_counter}/>
       </div>
