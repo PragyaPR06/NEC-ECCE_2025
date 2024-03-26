23339496a6eea0a00513e14f6998c168381a8c62
hradaysadrani <hradaysadrani@gmail.com>
2024-03-27 00:22:15 +0530

    changed size of logo and header info

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -26,7 +26,7 @@ const Header =() => {
           <img src={require("./ne-iecce-2025_main_logo.jpg")} alt="ne-iecce-2025_logo" width="150rem" height="100rem"/>
         </div>
         <div className={styles.header_logo}>
-          <img src={require("./nits-logo.png")} alt="nits-logo" width="110rem" height="80rem"/> 
+          <img src={require("./nits-logo.png")} alt="nits-logo" width="100rem" height="75rem"/> 
         </div>
         <div className={styles.header_counter}/>
       </div>
@@ -35,7 +35,7 @@ const Header =() => {
           <img src={require("./location-icon.png")} alt="location-icon"/>
         </div>
         <div className={styles.header_location_data}>
-          <h3 className={styles.pathway_extreme_header_location_data}> National Institute of Technology, Silchar, Assam | INDIA</h3>
+          <h3 className={styles.pathway_extreme_header_location_data}> National Institute of Technology Silchar, Assam | INDIA</h3>
           <h3 className={styles.pathway_extreme_header_location_dates}> 4-6 July 2025</h3>
         </div>
         <div className={styles.header_logos}>
