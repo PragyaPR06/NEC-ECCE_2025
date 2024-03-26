408a204c443824a574be3f1bacdbe79d2fa2ad23
hradaysadrani <hradaysadrani@gmail.com>
2024-03-27 00:08:28 +0530

    added nits and IEEE logo

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -20,13 +20,13 @@ const Header =() => {
         <div className={styles.pathway_extreme_header_info}>
           <h2>IEEE North-East India</h2>
           <h2>International Energy Conversion Conference and Exhibition</h2>
-          <h2>(NE-IECCE 2025)</h2>
+          <h2>(NEI-ECCE 2025)</h2>
         </div>
         <div className={styles.header_logo}>
           <img src={require("./ne-iecce-2025_main_logo.jpg")} alt="ne-iecce-2025_logo" width="150rem" height="100rem"/>
         </div>
         <div className={styles.header_logo}>
-          {/* <img src={require("./nits-logo.svg")} alt="nits-logo" width="150rem" height="100rem"/> */}
+          <img src={require("./nits-logo.png")} alt="nits-logo" width="150rem" height="100rem"/> 
         </div>
         <div className={styles.header_counter}/>
       </div>
@@ -49,7 +49,7 @@ const Header =() => {
             <img src={require("./ias-logo.jpeg")} alt="ias-logo"/>
           </div>
           <div className={styles.location_ieee_logo}>
-            {/* <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/IEEE_logo.svg" alt="ieee-main-logo"/> */}
+            <img src={require("./IEEE-main-logo.png")} alt="ieee-main-logo"/>
           </div>
         </div>
       </div>
