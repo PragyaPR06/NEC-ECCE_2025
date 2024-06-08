9f99492d796be198cd0f886aa51da49a913efb7d
BhargobLahon <lahonbhargab@gmail.com>
2024-06-08 20:00:00 +0530

    added sponsors and sponsorship page

---
+++ src/Components/Sponsors/Sponsors.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./Sponsors.module.scss";
+const Sponsor = () =>{
+    return(
+        <>
+            <div className={styles.TechnicalSponsors}>
+            <h1 className={styles.header}>
+                <b>
+                Technical Co-Sponsors
+                </b>
+            </h1>
+            <hr></hr>
+            <div className={styles.icons}>
+            <div className={styles.logo1}>
+          <img src={require("./logo1.jpeg")} alt="ne-iecce-2025_logo" width="200rem" height="200rem"/>
+        </div>
+        <div className={styles.logo2}>
+        <img src={require("./logo2.png")} alt="ne-iecce-2025_logo" width="200rem" height="200rem"/>
+        </div>
+        <div className={styles.logo3}>
+        <img src={require("./logo3.png")} alt="ne-iecce-2025_logo" width="190rem" height="190rem"/>
+        </div>
+        <div className={styles.logo4}>
+        <img src={require("./logo4.png")} alt="ne-iecce-2025_logo" />
+        </div>
+        <div className={styles.logo5}>
+        <img src={require("./logo5.png")} alt="ne-iecce-2025_logo" width="200rem" height="200rem"/>
+        </div>
+        <div className={styles.logo6}>
+        <img src={require("./logo6.png")} alt="ne-iecce-2025_logo" width="200rem" height="200rem"/>
+        </div>
+      
+            </div>
+            </div>
+
+            <div className={styles.FinancialSponsors}>
+            <h1 className={styles.header}>
+                <b>
+                Financial Co-Sponsors
+                </b>
+            </h1>  <hr></hr>
+            <div className={styles.icons}>
+            <div className={styles.logo1}>
+          <img src={require("./logo1.jpeg")} alt="ne-iecce-2025_logo" width="250rem" height="250rem"/>
+        </div>
+        <div className={styles.logo2}>
+        <img src={require("./logo5.png")} alt="ne-iecce-2025_logo" width="250rem" height="250rem"/>
+        </div>
+        </div>
+            </div>
+        </>
+    )
+}
+
+export default Sponsor;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,8 @@ import Navbar from "../Components/Navbar/Navbar"
 import AboutUsComponent from '../Components/About/About';
 import Timeline  from '../Components/Timeline/Timeline';
 import Timer from '../Components/Timer/Timer';
+import Sponsor from '../Components/Sponsors/Sponsors';
+
 
 
 import Header from '../Components/Header/header';
@@ -17,11 +19,12 @@ const Home = () => {
       <Navbar/>
       
       <HomeSlider />
+      <Sponsor/>
       <AboutUsComponent/>
       <Tracks/>
       <Timeline />
       <Timer/>
-
+      
       
     </div>
   )
