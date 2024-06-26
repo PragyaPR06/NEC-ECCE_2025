24edea01f6176a2b5ee060f11da77f92e3770f11
webdotmilan <milans708688@gmail.com>
2024-06-26 17:18:03 +0530

    changes scrolling  sublines also added committe memeber

--- src/Components/SubLine/ScrollingText.jsx
+++ src/Components/SubLine/ScrollingText.jsx
@@ -8,8 +8,11 @@ const ScrollingText = () => {
     <div className="scrolling-container">
       <div className="scrolling-text">
        <h3 className={`${styles.pathway_extreme_header_info}`}>
-          Note - <a href="/CallForPapers"> Last Date for Abstract submission is <strong>30<sup>th</sup> August 2024</strong>.</a>
-        </h3>
+       <div className='flex '>
+       <div className='  p-0 m-0 items-center'><img src="http://www.nits.ac.in/images/tnew.gif" alt="Notification" className=" items-center pt-1 " /></div>
+       <div style={{padding: 0}}>- <a href="/CallForPapers"> Paper Submission Opens On  <strong>30<sup>th</sup> August 2024</strong>.</a></div> 
+       </div>                                                                                                                                                                                                 
+         </h3> 
       </div>
     </div>
   );
