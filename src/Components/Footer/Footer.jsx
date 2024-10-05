f47a23690f5fecc1e55d1db68938f7e0a2d1bf23
Hraday Sadrani <113025213+hradaysadrani@users.noreply.github.com>
2024-10-05 15:22:46 +0530

    modified footer design and fixed its resolution bug, fixed registration page and sponsor section resolution bug, updated scrolling text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div className=' container flex justify-center items-center m-0 p-[2rem 0rem] h-[30vh] bg-cyan-400'>
-        <h2>
+    // <div className=' container flex justify-center items-center m-0  h-[30vh] bg-cyan-400'>
+      <footer className='footer'>
+        <section className='copyright-text'>
+        <h2 >
             Copyright &copy; 2025 NE-IECCE 
         </h2>
-    </div>
+        </section>
+        <section className=''>
+        {/* To be updated */}
+        </section>
+        </footer>
+    // </div>
   )
 };
 export default Footer;
