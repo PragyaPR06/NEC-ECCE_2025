a2c78bf39ffa65c9c17f9840bd7f5ccc5b411039
BhargobLahon <lahonbhargab@gmail.com>
2024-12-19 22:07:00 +0530

    adding special tracks and accomodation

--- src/Components/Tracks/Tracks.jsx
+++ src/Components/Tracks/Tracks.jsx
@@ -42,6 +42,53 @@ const Tracks = () => {
         
         </div>
       </div>
+      <br></br><br></br>
+      <div className={styles.header}>
+        <h1>
+          <b>Special Session<span> Tracks</span></b>
+        </h1>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 1 :</h3>
+            <h2>Advances in Control and Operation of Inverter-Dominated Power Distribution System</h2>
+          </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 2 :</h3>
+            <h2>Protection Solutions for Inverter Based Resource Dominated Smart Power System (PS-IBR-SPS)</h2>
+          </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 3:</h3>
+            <h2>Transformative Solar Energy Solutions for Next-Generation Power and Mobility
+</h2>
+          </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 7:</h3>
+            <h2> Advances in Wired and Wireless Charging Infrastructure for Sustainable Vehicular Technologies</h2>
+          </div>
+          
+         
+        </div>
+        <div className={styles.right}>
+          
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 4:</h3>
+            <h2>Stationary and Mobile battery storage for Advancing grid
+            Resilience and flexibiliTy </h2>
+          </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 5:</h3>
+            <h2> Artificial Intelligence and Machine Learning for Smart, Resilient, and Sustainable Energy Systems</h2>
+          </div>
+          <div className={styles.lcard}>
+            <h3 className=" font-semibold">SS 6:</h3>
+            <h2>Hydrogen Fuel Cells and Hybrid Electric Vehicles: Innovations in Control, Design, and Policy for Sustainable e-Mobility.</h2>
+          </div>
+         
+        
+        </div>
+      </div>
     </div>
   );
 };
