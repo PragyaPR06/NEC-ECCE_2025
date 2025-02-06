8cfb18cde2d73450460a2133ed88be32c6c1bdb0
Hraday Sadrani <113025213+hradaysadrani@users.noreply.github.com>
2025-02-06 12:49:45 +0530

    Updated IAS Logo, Added content on Student Awards page and rectified bug of text overflow on contactUs Page

--- src/Pages/Awards/StudentAwards.jsx
+++ src/Pages/Awards/StudentAwards.jsx
@@ -1,20 +1,30 @@
 import React, { Fragment } from 'react'
 import Header from '../../Components/Header/header'
 import Navbar from '../../Components/Navbar/Navbar'
-import styles from './Awards.module.css'
+import styles from './Awards.module.scss'
 
-const StudentAwards = () => {
-  return (
-    <Fragment>
- <Header/>
- <Navbar/>
- <section className={styles.header}>
-            <br /><br />
-                <h1>Coming <span>Soon !!</span></h1>
-                <br /><br /><br />
-            </section>
-    </Fragment>
+const StudentAwards = () =>{
+  return(
+      <>
+      <Header/>
+      <Navbar />
+      
+
+          <div className={styles.studentAwardSection}>
+              <img src="/Awards/awards_flyer.jpeg" alt="Student Awards Flyer" ></img></div><br />
+              <div className="text-center">
+<h2 className="font-bold text-blue-500 md:text-2xl text-[clamp(10px,3vw,18px)] mb-4">
+  For more information, download the docx file from button below:
+</h2>
+<a href="/Awards/StudentTravelAward.docx" download>
+  <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+    Download
+  </button>
+</a>
+</div>
+<br></br>
+      </>
   )
-}
+};
 
-export default StudentAwards
+export default StudentAwards;
